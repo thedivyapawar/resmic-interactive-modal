96630ff49a3c2ab2b38e442cf5b1af9897f49d23
Divya Pawar <divyapawar2033@gmail.com>
2025-01-27 03:21:52 +0530

    updated UI

--- src/components/PopUpModal.jsx
+++ src/components/PopUpModal.jsx
@@ -36,7 +36,7 @@ function PopUpModal() {
         <div className="row min-vh-100">
             <div className="col-12 d-flex justify-content-center align-items-center flex-wrap">
                 <div className="row">
-                    <div className="col-md-5 m-3 d-flex justify-content-center">
+                    <div className="col-md-5 m-3">
                         <button className="btn mx-4" data-bs-toggle="modal" data-bs-target="#exampleModal">
                         <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" className="sparkle">
                             <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
@@ -45,7 +45,7 @@ function PopUpModal() {
                         </button>
                     </div>
 
-                    <div className="col-md-5 m-3 d-flex justify-content-center">
+                    <div className="col-md-5 m-3">
                         <button className="btn mx-4" onClick={handleOpen}>
                         <svg height="24" width="24" fill="#FFFFFF" viewBox="0 0 24 24" data-name="Layer 1" id="Layer_1" className="sparkle">
                             <path d="M10,21.236,6.755,14.745.264,11.5,6.755,8.255,10,1.764l3.245,6.491L19.736,11.5l-6.491,3.245ZM18,21l1.5,3L21,21l3-1.5L21,18l-1.5-3L18,18l-3,1.5ZM19.333,4.667,20.5,7l1.167-2.333L24,3.5,21.667,2.333,20.5,0,19.333,2.333,17,3.5Z"></path>
